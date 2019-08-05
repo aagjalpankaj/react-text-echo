cae9ffa64fd2238037223fc31fb814836b840e73
Pankaj Aagjal <aagjalpankaj@gmail.com>
2019-08-05 22:25:17 +0530

    Updates

---
+++ precache-manifest.dc589e472f3b7518d43ed17e6b270f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a423843d347913cd262c4a662ebcbd5",
+    "url": "/react-text-echo/index.html"
+  },
+  {
+    "revision": "3148a004f903ec72e3ba",
+    "url": "/react-text-echo/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3148a004f903ec72e3ba",
+    "url": "/react-text-echo/static/js/2.b0f4c8b7.chunk.js"
+  },
+  {
+    "revision": "b154906e5544fb48c702",
+    "url": "/react-text-echo/static/js/main.dfbec4a7.chunk.js"
+  },
+  {
+    "revision": "7902a24771686f8a04b0",
+    "url": "/react-text-echo/static/js/runtime~main.7504ba96.js"
+  }
+]);
